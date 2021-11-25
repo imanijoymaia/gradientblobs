d6df3bf48d7c7694f3250c053ba9986681b736b9
imanijoymaia <95016394+imanijoymaia@users.noreply.github.com>
2021-11-25 00:08:36 -0800

    Add saturation effect

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ class App {
     this.canvas.width = this.stageWidth * this.pixelRatio;
     this.canvas.height = this.stageHeight * this.pixelRatio;
     this.ctx.scale(this.pixelRatio, this.pixelRatio);
-    
+
     this.ctx.globalCompositeOperation = 'saturation';
 
     this.createParticles();
